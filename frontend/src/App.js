62f919c9198f696c336f9db871fb7310da1b5da1
DinuZk <dinuzelinschi@gmail.com>
2025-01-20 23:30:58 +0200

    Rezolvare conflicte în App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,11 +5,7 @@ import AuthComponent from './components/AuthComponent';
 import Dashboard from './components/Dashboard';
 import QRCodeScannerComponent from './components/QRCodeScanner';
 import ExportButton from './components/ExportButton';
-<<<<<<< HEAD
 import FormEvent from './components/FormEvent'; // exemplu
-=======
-import FormEvent from './components/FormEvent';
->>>>>>> 526c1d07f0cd91fb53d1c31af4db4de1b64d6df3
 
 function App() {
   const [authToken, setAuthToken] = useState(null);
@@ -17,7 +13,6 @@ function App() {
   return (
     <Router>
       <Routes>
-<<<<<<< HEAD
         <Route
           path="/"
           element={<AuthComponent setAuthToken={setAuthToken} />}
@@ -58,13 +53,6 @@ function App() {
             )
           }
         />
-=======
-        <Route path="/" element={<AuthComponent setAuthToken={setAuthToken} />} />
-        <Route path="/dashboard" element={authToken ? <Dashboard /> : <AuthComponent setAuthToken={setAuthToken} />} />
-        <Route path="/scan-qr" element={authToken ? <QRCodeScannerComponent /> : <AuthComponent setAuthToken={setAuthToken} />} />
-        <Route path="/export/:evenimentId" element={authToken ? <ExportButton evenimentId={1} /> : <AuthComponent setAuthToken={setAuthToken} />} />
-        <Route path="/add-event" element={authToken ? <FormEvent /> : <AuthComponent setAuthToken={setAuthToken} />} />
->>>>>>> 526c1d07f0cd91fb53d1c31af4db4de1b64d6df3
       </Routes>
     </Router>
   );
